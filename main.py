01bcf4cde72bf960b899fb79b03e900d49b8206d
AndrewUryvaev <117014010+AndrewUryvaev@users.noreply.github.com>
2023-03-18 13:32:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,63 @@
 import re
-
-from pprint import pprint
-
 import csv
 
-with open("phonebook_raw.csv", encoding="utf-8") as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
-
-phone_pattern = r'(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})[\s\(]*(доб\.)*[\s]*(\d+)*[\)]*'
-phone_sub = r'+7(\2)-\3-\4-\5 \6\7'
-
-def main(contact_list: list):
-    new_list = list()
-    for i in contact_list:
-        full_name = ' '.join(i[:3]).split(' ')
-        result = [full_name[0], full_name[1], full_name[2], i[3], i[4], re.sub(phone_pattern, phone_sub, i[5]), i[6]]
-        new_list.append(result)
-    return combined(new_list)
-
-def combined(contacts: list):
-    for contact in contacts:
-        first_name = contact[0]
-        last_name = contact[1]
-        for app_contact in contacts:
-            app_first_name = app_contact[0]
-            app_last_name = app_contact[1]
-            if first_name == app_first_name and last_name == app_last_name:
-                if contact[2] == "": contact[2] = app_contact[2]
-                if contact[3] == "": contact[3] = app_contact[3]
-                if contact[4] == "": contact[4] = app_contact[4]
-                if contact[5] == "": contact[5] = app_contact[5]
-                if contact[6] == "": contact[6] = app_contact[6]
-        result_list = list()
-        for i in contacts:
-            if i not in result_list:
-                result_list.append(i)
-
-        return result_list
+DATA_FILE = 'phonebook_raw.csv'
+PHONE_PATTERN = '(8|\+7)?\s*(\(*)(\d{3})(\)*)(\s*|-)(\d{3})(\s*|-)(\d{2})(\s*|-)(\d{2})\s*(\(*)(\w\w\w\.)*\s*(\d{4})*(\))*'
+SUB_PHONE = r'+7(\3)\6-\8-\10 \12\13'
 
 
-with open("phonebook.csv", "w", encoding="utf-8") as f:
-    datawriter = csv.writer(f, delimiter=',')
-    datawriter.writerows(main(contacts_list))
+def input_data():
+    with open(DATA_FILE, encoding='utf-8') as f:
+        rows = csv.reader(f, delimiter=",")
+        contacts_list = list(rows)
+    return contacts_list
 
+
+def parse_contact_list(contacts_list):
+    new_contacts_list = list()
+    for contact in contacts_list:
+        new_contact = list()
+        full_name_str = ",".join(contact[:3])
+        result = re.findall(r'(\w+)', full_name_str)
+        while len(result) < 3:
+            result.append('')
+        new_contact += result
+        new_contact.append(contact[3])
+        new_contact.append(contact[4])
+        phone_pattern = re.compile(PHONE_PATTERN)
+        changed_phone = phone_pattern.sub(SUB_PHONE, contact[5])
+        new_contact.append(changed_phone)
+        new_contact.append(contact[6])
+        new_contacts_list.append(new_contact)
+    return new_contacts_list
+
+
+def delete_duplicates_contact(new_contacts_list):
+    phone_book = dict()
+    for contact in new_contacts_list:
+        if contact[0] in phone_book:
+            contact_value = phone_book[contact[0]]
+            for i in range(len(contact_value)):
+                if contact[i]:
+                    contact_value[i] = contact[i]
+        else:
+            phone_book[contact[0]] = contact
+    return list(phone_book.values())
+
+
+def write_data(new_contacts_list):
+    with open("phonebook.csv", "w", newline='', encoding='utf-8') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(new_contacts_list)
+
+
+new_contacts_list = input_data()
+
+new_parsed_list = parse_contact_list(new_contacts_list)
+
+contact_book_values = delete_duplicates_contact(new_parsed_list)
+
+write_data(contact_book_values)
 
 
 
